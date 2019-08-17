0712779df14f3546a7c3a558d75ff15d1ece1a9e
ramsysproject <m.ramirez05@gmail.com>
2019-08-17 06:34:15 -0300

    Refactor code to map player throws to pin numbers

--- src/main/java/com/emramirez/bowling/processor/ScoreFileParser.java
+++ src/main/java/com/emramirez/bowling/processor/ScoreFileParser.java
@@ -12,20 +12,37 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
 
 public class ScoreFileParser {
 
     public Map parseScoreFile(Path scoreFilesPath) throws IOException {
         Stream<String> scoreLines = Files.lines(scoreFilesPath);
 
-        Map<Player, List<String>> playerFrames = scoreLines
+        Map<Player, List<Integer>> playerFrames = scoreLines
                 .filter(not(String::isEmpty))
-                .collect(Collectors.groupingBy((String scoreLine) -> {
-                    return Arrays.stream(scoreLine.split("\\s+")).findFirst().map(Player::new).get();
-                }));
+                .collect(
+                    Collectors.groupingBy(
+                        ScoreFileParser::extractPlayer,
+                        mapping(ScoreFileParser::extractPins, toList())
+                    )
+                );
 
         System.out.println(playerFrames);
 
         return playerFrames;
+    }
+
+    private static Player extractPlayer(String scoreLine) {
+        return Arrays.stream(scoreLine.split("\\s+")).findFirst().map(Player::new).get();
+    }
+
+    private static Integer extractPins(String scoreLine) {
+        return Arrays.stream(scoreLine.split("\\s+"))
+                .skip(1)
+                .map(s -> s.replace("F", "0"))
+                .findFirst()
+                .map(Integer::valueOf).get();
     }
 }
